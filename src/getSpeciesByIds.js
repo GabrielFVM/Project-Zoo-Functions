96f0e174d02fd45ea3fcb0125ff5820ac8f5eb02
GabrielFV <liifon@AngelEden.localdomain>
2022-08-15 17:52:06 -0300

    segundo e terceiro requisito :rocket:

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -6,5 +6,5 @@ function getSpeciesByIds(...ids) {
     .filter((specie) => ids
       .includes(specie.id));
 }
-console.log(getSpeciesByIds());
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
