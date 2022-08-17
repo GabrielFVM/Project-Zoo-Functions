92035f2cab85e02bfb43b5ca1dd4f4c2da82ecce
GabrielFV <liifon@AngelEden.localdomain>
2022-08-17 15:03:13 -0300

    requisito 4 :rocket:

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  const worker = employees
+    .find((employee) => (employee.id === id));
+  return employees
+    .some((manager) => manager.managers
+      .includes(worker.id));
 }
-
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees
+    .filter((employee) => employee.managers
+      .includes(managerId))
+    .map((employee) => `${employee.firstName} ${employee.lastName}`);
 }
-
+console.log(getRelatedEmployees('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 module.exports = { isManager, getRelatedEmployees };
